c11244de6e6e4c8d72916848917813d1c63a5edb
StepanHovsepyan7 <stpnhovsepyan@gmail.com>
2025-06-02 21:13:18 +0400

    added category buttons

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import Cards from './components/Cards';
 import { Link } from 'react-router-dom';
+import Categories from './components/Categories';
 
 
 function App({ inpValue }) {
@@ -29,6 +30,14 @@ function App({ inpValue }) {
 
   return (
     <div className="App">
+
+      <div className="max-w-[1440px] m-auto p-3 pt-[100px]">
+        <h3 className='text-[50px] text-center pt-[30px]'>Categories</h3>
+        <div className='pt-10'>
+          <Categories />
+        </div>
+      </div>
+
       <h1 className='text-[50px] text-center pt-[60px]'>Products</h1>
       <div className='max-w-[1440px] m-auto p-3'>
         <div className='flex justify-center flex-wrap items-center gap-[20px] text-center mt-5'>
--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-function Categories({category}) {
-  return (
-    <div>
-        <button>{category}</button>
-    </div>
-  )
+function Categories() {
+
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch('https://dummyjson.com/products/category-list')
+            .then(res => res.json())
+            .then(res => setCategories(res));
+    }, [])
+
+
+    
+    return (
+        <div>
+            <div className='flex flex-wrap justify-center items-center gap-[20px]'>
+                {categories.map((category, i) => {
+                    return (
+                        <div  key={i}>
+                            <button  className='categoriesBtn'>{category}</button>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    )
 }
 
 export default Categories
